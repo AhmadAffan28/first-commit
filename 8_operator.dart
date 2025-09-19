dfde4629920681f388369f3808688df4ba343883
AhmadAffan28 <ahmad.aff4n@gmail.com>
2025-09-19 10:48:35 +0700

    baru belajar operator

---
+++ 8_operator.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  /**
+   * namberOne Penulisan denga
+   * camel case
+   * namber_one penulisan dengan
+   * snake case
+   */
+  
+  var numeberOne = 10;
+  var numeberTwo = 20;
+
+}
