a47d62619109daa9919f605ff378c4fb359f1212
AhmadAffan28 <ahmad.aff4n@gmail.com>
2025-10-09 14:10:11 +0700

    belajar inheritance

---
+++ oop_2/cat.dart
@@ -0,0 +1,20 @@
+import 'hewan.dart';
+
+/**
+ * gunakan kata kunci extends untuk
+ * menjadikan kelas turunan (subclass)
+ * dari kelas parent (superclass)
+ */
+
+class Cat extends Hewan {
+  // properties
+  String? fourColor;
+
+  // contructor
+  Cat(this.fourColor) : super('', 0.0, 0);
+
+  // method
+  void walk() {
+    print('$name is walking');
+  }
+}
